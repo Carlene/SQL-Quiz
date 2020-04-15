c66c278ae6e43c7a29a70a3d607f68c44a3e16cd
Carlene Williams <carlenew.27@gmail.com>
2020-04-15 13:17:45 -0400

    Wrote a query for Question 2

--- answers.sql
+++ answers.sql
@@ -8,4 +8,19 @@ where sno NOT IN (
 	where cno = 'CS112'
 	)
 
+
 # Q2: Find all student numbers for students who take a course other than CS112.
+
+select sno, sname
+from (
+	select s.sno, sname, cno
+	from student as s
+	left join take as t
+	on s.sno = t.sno
+	) as all_classes
+
+where cno != 'CS112'
+
+group by sno, sname
+
+# Q3: Which students take at least three courses?
