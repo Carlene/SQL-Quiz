5587f77476c4d2314be11d5e5578a551ec029c26
Carlene Williams <carlenew.27@gmail.com>
2020-04-16 11:28:30 -0400

    Wrote queries for questions 5+6

--- answers.sql
+++ answers.sql
@@ -14,6 +14,7 @@ where sno NOT IN (
 select sno, sname
 from (
 	select s.sno, sname, cno
+
 	from student as s
 	left join take as t
 	on s.sno = t.sno
@@ -26,15 +27,78 @@ group by sno, sname
 # Q3: Which students take at least three courses?
 
 with amount_classes as (
-	select sno, count(cno) as classes
+	select 
+	sno, count(cno) as classes
 	from take
 	group by sno
 	)
 
 select s.sno, sname, classes
+
 from student as s
 join amount_classes as ac
 on s.sno = ac.sno
 
 where classes >= 3
+
+# Q4: Find students who take CS112 or CS114 but not both.
+select sno, sname, more_than_one
+from (
+	select 
+	*
+	,row_number () over (partition by sno) as more_than_one
+
+	from (
+		select s.sno, sname, cno
+
+		from student as s
+		join take as t
+		on s.sno = t.sno
+
+		where cno = 'CS112'
+		OR 
+		cno = 'CS114'
+		) as both_classes
+	) as class_list
+
+where more_than_one = 1
+
+-- Q5: Find the students who take exactly 2 courses.
+
+with amount_classes as (
+	select 
+	sno, count(cno) as classes
+	from take
+	group by sno
+	)
+
+select s.sno, sname, classes
+
+from student as s
+join amount_classes as ac
+on s.sno = ac.sno
+
+where classes = 2
+
+-- Q6: Find the students who take at most 2 courses.
+
+with amount_classes as (
+	select 
+	sno, count(cno) as classes
+	from take
+	group by sno
+	)
+
+select s.sno, sname, classes
+
+from student as s
+join amount_classes as ac
+on s.sno = ac.sno
+
+where classes <=2
+
+-- Q7: Find the students who take only CS112 and nothing else.
+-- Q8: Find the youngest students WITHOUT using MIN() or MAX().
+
+
 
